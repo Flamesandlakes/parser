3e69986fae1ae2eca45e33cfbf96005d379361e4
Daniel Ilsoe <danibenjakarl@gmail.com>
2026-09-14 12:39:29 +0200

    Nested dict format step added

--- prototype.py
+++ prototype.py
@@ -1,12 +1,22 @@
 # Importing necessary libraries
 import os 
+import json
 
 test_file_paths = ["data/employees.ascii.csv", "data/sogne.dawa.csv"]
 
-# with os.open("data/employees.ascii.csv", "r") as file:
-#     content = file.readlines()
-#     print(content)
+with open("data/employees.ascii.csv", "r") as file:
+    content = file.read()#.replace('\n', '')
+    #print(content)
+    headers = [head.strip() for head in content.split('\n')[0].split(',')]
+    #print(headers)
 
-# with os.open("data/employees.ascii.csv", os.O_RDONLY) as file:
-#     content = file.read()
-#     print(content)
+    # dict format /hashmap (for json)
+    idx_var = 0 # mulighed for at specificere en anden index variabel
+
+    rows = [line for line in content.split('\n')[1:] if line]
+
+    entries = {}
+    for row in rows:
+        idx = row.split(',')[idx_var] # sæt index 
+        entries[idx] = {head: value for head, value in zip(headers, row.split(','))} # find værdierne for det givne index, opstillet som dict (inkl. index selv)
+    print(entries)
