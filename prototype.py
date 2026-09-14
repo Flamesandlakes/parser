3bb898c3c47ecad9706ce9c2b4d2345f31f5bf7f
Daniel Ilsoe <danibenjakarl@gmail.com>
2026-09-14 14:51:13 +0200

    Implemented index_variable  functionality for JSON output structure

--- prototype.py
+++ prototype.py
@@ -5,20 +5,18 @@ import json
 test_file_paths = ["data/employees.ascii.csv", "data/sogne.dawa.csv"]
 
 class Parser():
-    def __init__(self, input_file_path, output_file_path="outputs/placeholder.json", group_by_headers=False, ignore_placeholder=False):
+    def __init__(self, input_file_path, output_file_path="outputs/placeholder.json", index_variable = None, ignore_placeholder=False):
         # input_file_path is the path to the input file (including file name and type)
         # output_file_path is the path where the resulting file will be saved (without file name)
+        # index_variable is the index variable to be used for the JSON output. If None, unique numbers will be used.
         # ignore_placeholder is a boolean indicating whether to ignore the use of the placeholder output file path. 
             # If False, the user will be prompted to enter a output file path. If False, the placeholder path will just be used.
 
-        # Potential future features:
-        # group_by_headers is a boolean indicating whether to group entries by their headers. A switch corresponds to transposing the data structure.
-            # When False, the entries are indexed by row. When True, the entries are values for each header.
-        
         self.input_file_path = input_file_path
         self.output_file_path = output_file_path
-        self.group_by_headers = group_by_headers
+        self.index_variable = index_variable
         self.ignore_placeholder = ignore_placeholder
+        
 
         if self.output_file_path == "outputs/placeholder":
                     self.output_file_path = input("Please enter the output file path (must end with .json): ")
@@ -31,25 +29,30 @@ class Parser():
             content = file.read()#.replace('\n', '')
             #print(content)
             headers = [head.strip() for head in content.split('\n')[0].split(',')]
-            #print(headers)
+            if self.index_variable is not None and self.index_variable not in headers:
+                raise ValueError(f"Index variable '{self.index_variable}' not found in headers: {headers}")
+            elif self.index_variable is not None:
+                idx_variable_location = headers.index(self.index_variable) 
 
             # dict format /hashmap (for json)
-            idx_var = 0 # mulighed for at specificere en anden index variabel
-
             rows = [line for line in content.split('\n')[1:] if line]
 
-            entries = {}
-            for row in rows:
-                idx = row.split(',')[idx_var] # sæt index 
-                entries[idx] = {head: value for head, value in zip(headers, row.split(','))} # find værdierne for det givne index, opstillet som dict (inkl. index selv)
-            print(entries)
+            entries = {} # init dict til at holde entries
+            
+            for idx, row in enumerate(rows):
+                values = {head: value for head, value in zip(headers, row.split(','))} # find værdierne for det givne index, opstillet som dict (inkl. eventuel index selv)
+                if self.index_variable is None:
+                    entries[idx] = values
+                else:
+                    entries[row.split(',')[idx_variable_location]] = values
 
         json_output = json.dumps(entries)
 
         with open(self.output_file_path, "w") as file:
             file.write(json_output)
 
+            
 if __name__ == "__main__": # sørger for at koden ikke executes når den blot importeres som modul
-    parser = Parser("data/employees.ascii.csv", output_file_path="outputs/employees.json", group_by_headers=False, ignore_placeholder=False)
+    parser = Parser("data/employees.ascii.csv", output_file_path="outputs/employees.json", ignore_placeholder=False)
     parser.parse_to_JSON()
 
