46349d3eaaeeb3e18e8106a01e8c3792f5e3ecb0
Daniel Ilsoe <danibenjakarl@gmail.com>
2026-09-14 11:31:02 +0200

    prototype init

---
+++ prototype.py
@@ -0,0 +1,12 @@
+# Importing necessary libraries
+import os 
+
+test_file_paths = ["data/employees.ascii.csv", "data/sogne.dawa.csv"]
+
+# with os.open("data/employees.ascii.csv", "r") as file:
+#     content = file.readlines()
+#     print(content)
+
+# with os.open("data/employees.ascii.csv", os.O_RDONLY) as file:
+#     content = file.read()
+#     print(content)
